b519e4ca04e91398d872915d5271ea5ead6ed499
Shakhawathh <shakhawathh55@gmail.com>
2022-05-26 14:48:18 +0600

    jwtTokne

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
@@ -22,7 +23,23 @@ async function run() {
         await client.connect()
         const serviceCollection = client.db('bike_parts').collection('services')
         const bookingCollection = client.db('bike_parts').collection('bookings')
+        const userCollection = client.db('bike_parts').collection('users')
 
+
+
+
+        app.put('/user/:email', async(req, res) =>{
+            const email = req.params.email
+            const user = req.body
+            const filter={email:email}
+            const options={upsert:true}
+            const updateDoc ={
+                $set:user,
+            }
+            const result = await userCollection.updateOne(filter,updateDoc,options)
+            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+            res.send({result ,token})
+        })
 
         app.get('/booking', async (req, res) => {
             const email = req.query.email
