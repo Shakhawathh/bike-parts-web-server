8b0c8ef44a0a3f9efeeb268858d44c36feb9cc77
Shakhawathh <shakhawathh55@gmail.com>
2022-05-26 17:51:00 +0600

    admin role

--- index.js
+++ index.js
@@ -43,10 +43,21 @@ async function run() {
 
 
 
-      app.get('/user',verifyJWT, async(req,res)=>{
-          const users = await userCollection.find().toArray()
-          res.send(users)
-      })
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+
+
+        app.put('/user/:admin/:email',verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: 'admin' },
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
 
 
 
@@ -71,8 +82,8 @@ async function run() {
                 const bookings = await bookingCollection.find(query).toArray()
                 res.send(bookings)
             }
-            else{
-                return res.status(403).send({message: 'forbidden access'})
+            else {
+                return res.status(403).send({ message: 'forbidden access' })
             }
 
         })
